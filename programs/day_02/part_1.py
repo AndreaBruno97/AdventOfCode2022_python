a289fa51a3f4f9a3818b33535214dbb1c973bc3d
AndreaBruno97 <andbruno97@gmail.com>
2022-12-04 14:10:32 +0100

    Day 2

--- programs/day_02/part_1.py
+++ programs/day_02/part_1.py
@@ -1,18 +1,76 @@
 from common import *
 
 
+class Move_list:
+    def __init__(self, rock, paper, scissors):
+        self.rock = rock
+        self.paper = paper
+        self.scissors = scissors
+
+
 class Part_1(BaseClass):
 
     def __init__(self):
         super().__init__(2)
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    # Rock:         A (opponent), X (me)
+    # Paper:        B (opponent), Y (me)
+    # Scissors:     C (opponent), Z (me)
+    opponent_move_list = Move_list("A", "B", "C")
+    my_move_list = Move_list("X", "Y", "Z")
 
-        return -1
+    # Loss: 0 points
+    # Tie: 3 points
+    # Win: 6 points
+    loss_points = 0
+    tie_points = 3
+    win_points = 6
+
+    # Bonus points
+    # Rock: 1 points
+    # Paper: 2 points
+    # Scissors: 3 points
+    bonus_points = {
+        my_move_list.rock: 1,
+        my_move_list.paper: 2,
+        my_move_list.scissors: 3
+    }
+
+    # 2-level dictionary:
+    #   - first key: opponent's move
+    #   - second key: my move
+    #   - value: result in points
+    resultDictionary = {
+        opponent_move_list.rock: {
+            my_move_list.rock: tie_points,
+            my_move_list.paper: win_points,
+            my_move_list.scissors: loss_points
+        },
+        opponent_move_list.paper: {
+            my_move_list.rock: loss_points,
+            my_move_list.paper: tie_points,
+            my_move_list.scissors: win_points
+        },
+        opponent_move_list.scissors: {
+            my_move_list.rock: win_points,
+            my_move_list.paper: loss_points,
+            my_move_list.scissors: tie_points
+        }
+    }
+
+    def execute_internal(self, filepath):
+        round_list = open_file_lines(filepath)
+        points = 0
+
+        for cur_round in round_list:
+            opponent_move, my_move = cur_round.split(" ")
+            points += self.resultDictionary[opponent_move][my_move]
+            points += self.bonus_points[my_move]
+
+        return points
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(15)
 p1.execute()
 
--- programs/day_02/part_2.py
+++ programs/day_02/part_2.py
@@ -6,13 +6,72 @@ class Part_2(BaseClass):
     def __init__(self):
         super().__init__(2)
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    # Rock:         A (opponent), X (me)
+    # Paper:        B (opponent), Y (me)
+    # Scissors:     C (opponent), Z (me)
+    opponent_move_list = {
+        "rock": "A",
+        "paper": "B",
+        "scissors": "C"
+    }
+    round_result_list = {
+        "loss": "X",
+        "tie": "Y",
+        "win": "Z"
+    }
 
-        return -1
+    # Loss: 0 points
+    # Tie: 3 points
+    # Win: 6 points
+    round_result_points = {
+        round_result_list["loss"]: 0,
+        round_result_list["tie"]: 3,
+        round_result_list["win"]: 6
+    }
+
+    # Bonus points
+    # Rock: 1 points
+    # Paper: 2 points
+    # Scissors: 3 points
+    bonus_points_rock = 1
+    bonus_points_paper = 2
+    bonus_points_scissors = 3
+
+    # 2-level dictionary:
+    #   - first key: opponent's move
+    #   - second key: my move
+    #   - value: result in points
+    resultDictionary = {
+        opponent_move_list["rock"]: {
+            round_result_list["loss"]: bonus_points_scissors,
+            round_result_list["tie"]: bonus_points_rock,
+            round_result_list["win"]: bonus_points_paper
+        },
+        opponent_move_list["paper"]: {
+            round_result_list["loss"]: bonus_points_rock,
+            round_result_list["tie"]: bonus_points_paper,
+            round_result_list["win"]: bonus_points_scissors
+        },
+        opponent_move_list["scissors"]: {
+            round_result_list["loss"]: bonus_points_paper,
+            round_result_list["tie"]: bonus_points_scissors,
+            round_result_list["win"]: bonus_points_rock
+        }
+    }
+
+    def execute_internal(self, filepath):
+        round_list = open_file_lines(filepath)
+        points = 0
+
+        for cur_round in round_list:
+            opponent_move, my_result = cur_round.split(" ")
+            points += self.resultDictionary[opponent_move][my_result]
+            points += self.round_result_points[my_result]
+
+        return points
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(12)
 p2.execute()
 
