4c3543e7d426f9cbfce59ed18fa467494191b11c
AndreaBruno97 <andbruno97@gmail.com>
2022-12-04 15:34:38 +0100

    Day 4

--- days/day_04/programs/part_1.py
+++ days/day_04/programs/part_1.py
@@ -1,17 +1,36 @@
 from common import *
 
 
+def is_fully_contained(pair_a, pair_b) -> bool:
+    return pair_a[0] >= pair_b[0] and pair_a[1] <= pair_b[1]
+
+
 class Part_1(BaseClass):
 
     def __init__(self):
         super().__init__()
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    # true if pair_a is fully contained in pair_b
 
-        return -1
+    def execute_internal(self, filepath):
+        assignment_list = open_file_lines(filepath)
+
+        overlapping_count = 0
+
+        for assignment_pair in assignment_list:
+            assignment_1, assignment_2 = assignment_pair.split(",")
+            assignment_1 = [int(x) for x in assignment_1.split("-")]
+            assignment_2 = [int(x) for x in assignment_2.split("-")]
+
+            if(
+                    is_fully_contained(assignment_1, assignment_2) or
+                    is_fully_contained(assignment_2, assignment_1)
+            ):
+                overlapping_count += 1
+
+        return overlapping_count
 
 
 p1 = Part_1()
-p1.test(0)
+p1.test(2)
 p1.execute()
--- days/day_04/programs/part_2.py
+++ days/day_04/programs/part_2.py
@@ -1,17 +1,33 @@
 from common import *
 
 
+def is_overlapped(pair_a, pair_b) -> bool:
+    return pair_a[1] >= pair_b[0] and pair_a[0] <= pair_b[1]
+
+
 class Part_2(BaseClass):
 
     def __init__(self):
         super().__init__()
 
-    def execute_internal(self, filepath):
-        print(open_file(filepath))
+    # true if pair_a is fully contained in pair_b
 
-        return -1
+    def execute_internal(self, filepath):
+        assignment_list = open_file_lines(filepath)
+
+        overlapping_count = 0
+
+        for assignment_pair in assignment_list:
+            assignment_1, assignment_2 = assignment_pair.split(",")
+            assignment_1 = [int(x) for x in assignment_1.split("-")]
+            assignment_2 = [int(x) for x in assignment_2.split("-")]
+
+            if is_overlapped(assignment_1, assignment_2):
+                overlapping_count += 1
+
+        return overlapping_count
 
 
 p2 = Part_2()
-p2.test(0)
+p2.test(4)
 p2.execute()
